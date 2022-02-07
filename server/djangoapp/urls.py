e28ccbc005563230943ea8c994e34692b0a87f6f
Elecbound <elecboy2@hotmail.com>
2022-02-06 19:24:24 -0500

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,19 +10,22 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path(route='aboutus/', view=views.about, name='aboutus'),
     # path for contact us view
+    path(route='contactus/', view=views.contact, name='contactus'),
 
     # path for registration
-
+    path('registration/', views.registration_request, name='registration'),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
+    path(route='reviews', view=views.about, name='reviews'),
     # path for add a review view
+    path(route='addreview', view=views.about, name='addreview'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
